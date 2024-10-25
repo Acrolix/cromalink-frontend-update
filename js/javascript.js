447efd0810668d2d27d11f12f2ea1c3b8d9d251f
no12232 <lucas.sancristibal@estudiante.ceibal.edu.uy>
2024-10-25 17:53:52 -0300

    Create javascript.js

---
+++ js/javascript.js
@@ -0,0 +1,42 @@
+let color = false;
+
+function cambiar_color(){
+    const contenido = document.querySelector('.contenido');
+    const sub_contenido = document.getElementsByClassName('sub-contenido');
+    const boton = document.querySelector('.boton-sub-contenido');
+    if(color === false){
+    contenido.style.backgroundColor = 'white';
+    contenido.style.color = 'black';
+
+    for(let i = 0; i < sub_contenido.length; i++){
+    sub_contenido[i].style.color = 'black';
+
+    boton.style.color = 'black';
+    boton.textContent = 'MODO OSCURO'
+    }
+    }
+    else{
+    contenido.style.backgroundColor = 'black';
+    contenido.style.color = '#ffffff';
+
+    for(let i = 0; i < sub_contenido.length; i++){
+    sub_contenido[i].style.color = '#ffffffea';
+
+    boton.style.color = '#ffffffea';
+    boton.textContent = 'MODO CLARO'
+    }
+    }
+    color = !color
+    localStorage.setItem('modo_oscuro', color.toString());
+}
+
+function guardado(){
+    const guardado = localStorage.getItem('modo_oscuro');
+
+    if(guardado !== null){
+        color = guardado === 'false';
+        cambiar_color();
+    }
+}
+
+window.onload = guardado;
